047bf52a655b5c27e11eefd053acacb270458011
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-26 01:38:13 +0100

    Update acvice.py

--- acvice.py
+++ acvice.py
@@ -1,20 +1,11 @@
-import streamlit as st
 import socket
-import qrcode
+import getpass
+import streamlit as st
 
-# Get the hostname
+# Get the hostname and username
 hostname = socket.gethostname()
+username = getpass.getuser()
 
-# Display the hostname in Streamlit
-st.write("Hostname:", hostname)
-
-# Generate a QR code for the page URL
-qr = qrcode.QRCode()
-qr.add_data(st.url)
-qr.make()
-
-# Get the QR code image as SVG
-qr_image = qr.make_image(image_factory=qrcode.image.svg.SvgImage)
-
-# Display the QR code in Streamlit
-st.image(qr_image)
+# Display the hostname and username in your Streamlit app
+st.write(f"Hostname: {hostname}")
+st.write(f"Username: {username}")
